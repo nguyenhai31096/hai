4cf8ffc646ff6a1aad69538cf88df2e047681e79
Nguyen Trong Hai <nguyenhai31096@gmail.com>
2020-07-14 16:16:17 +0700

    Update and rename check2.js to check3.js

---
+++ check3.js
@@ -0,0 +1,29 @@
+import fetch from 'isomorphic-fetch';
+
+window.SdkModule = (function() {
+
+  function httpGet(url) {
+    return fetch(url, {
+      headers: headers
+    })
+    .then(checkHttpStatus)
+  }
+
+  function init() {
+    console.log("init")
+    let url = `http://0.0.0.0:6800/api/notify/get_notify`
+    return httpGet(url)
+    .then(data => {
+      if (data.success) {
+        console.log(data)
+      }else {
+        console.log("false")
+      }
+    })
+    .catch(err => console.log(err))
+  }
+})();
+
+(function() {
+  window.psdk = SdkModule;
+})();
