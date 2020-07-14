f4b2e18535a2583d89911de7d064434beb9d380d
Nguyen Trong Hai <nguyenhai31096@gmail.com>
2020-07-14 14:40:43 +0700

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,8 +1,12 @@
+(function() {
+  window.psdk = SdkModule;
+})()
 
-window.sdk = {
-  getHostname: function() {console.log("test")}
-}
-
-// function() {
-//   return location.hostname !== "localhost" && location.hostname !== "local.docker" ? "http://api.susa.snappy.vn/api" : `http://${location.hostname}:6800/api`;
-// }
+const SdkModule = (function() {
+  function init() {
+    console.log("init")
+  }
+  return {
+    init
+  }
+})()
