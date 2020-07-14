be2e7c876d1754cc098d9101dc783e7788e6de77
Nguyen Trong Hai <nguyenhai31096@gmail.com>
2020-07-14 14:24:14 +0700

    Update sdk.js

--- sdk.js
+++ sdk.js
@@ -1,1 +1,10 @@
-console.log("checksdk")
+import axios from "axios"
+
+window.sdk = {
+  getHostname: function() {console.log("test")}
+}
+export {}
+
+// function() {
+//   return location.hostname !== "localhost" && location.hostname !== "local.docker" ? "http://api.susa.snappy.vn/api" : `http://${location.hostname}:6800/api`;
+// }
