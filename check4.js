d48299efcdf696242d74c1fd3402bc5b03ea1d2b
Nguyen Trong Hai <nguyenhai31096@gmail.com>
2020-07-15 16:37:40 +0700

    Update check4.js

--- check4.js
+++ check4.js
@@ -1,5 +1,33 @@
 window.SdkModule = (function() {
 
+  function getHostname() {
+    return location.hostname !== "localhost" && location.hostname !== "local.docker" ? "http://api.susa.snappy.vn/api" : `http://${location.hostname}:6800/api`;
+  }
+
+  function checkHttpStatus(response) {
+    if (response.status >= 200 && response.status < 300) {
+      return response.json();
+    } else {
+      var isJSON = true;
+      var result;
+  
+      try {
+        result = response.json();
+        result.success = false;
+      } catch (e) {
+        isJSON = false;
+      }
+  
+      if (isJSON) {
+        return result;
+      } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    }
+  }
+  
   function httpGet(url) {
     return fetch(url, {
       headers: headers
@@ -9,7 +37,7 @@ window.SdkModule = (function() {
 
   function init() {
     console.log("init")
-    let url = `http://0.0.0.0:6800/api/notify/get_notify`
+    let url = `${getHostname()}/notify/get_notify`
     return httpGet(url)
     .then(data => {
       if (data.success) {
